fa07c867e230f45f60855cfbe02f8a7fa384e7bd
thehackermonk <karthik.prakash1998@gmail.com>
2021-06-24 01:25:01 +0530

    URL check adds protocols to the URL and checks if those are directly accessible or not

--- urlcheck.py
+++ urlcheck.py
@@ -1,26 +1,53 @@
 import urllib.request
 
-with open('path_url_url_list_as_txt_file') as f:
-    mylist=list(f)
+## function open file
 
-for url in mylist:
+def open_file(file_name):
+    with open(file_name) as f:
+        mylist = list(f)
+        return mylist
 
-    # strip the url for unwanted characters
-    url=url.strip()
 
-    # complete the url by adding protocol to it, if needed
-    if(url.startswith('https://')):
-        url=url
-    elif(url.startswith('www')):
-        url='https://'+url
-    else:
-        url='https://www.'+url
+## function to append to file
 
+def write_file(text):
+    with open('output.txt', "a") as myfile:
+        myfile.write(text+"\n")
+
+
+## function to check url and return status
+
+def check_url(url):
     try:
-        # check for the url
-        if(urllib.request.urlopen(url)):
-            status=urllib.request.urlopen(url).getcode()
-            print(status,url)
+        if(urllib.request.urlopen(url, timeout=10)):
+            status = urllib.request.urlopen(url).getcode()
+            return status
     except:
         # catch the exception if the url returns an error
         print('Error: '+url)
+
+
+## path of input file
+input = '/home/thehackermonk/Bug Bounty/bitdefender/domains.txt'
+
+for url in open_file(input):
+    # strip the url for unwanted characters
+    url = url.strip()
+
+    # complete the url by adding protocol to it, if needed
+    if not url.startswith('https://') or not url.startswith('http://'):
+
+        status = check_url('https://'+url)
+        if status is not None:
+            write_file('https://'+url)
+            print('Success: '+'https://'+url)
+
+        status = check_url('http://'+url)
+        if status is not None:
+            write_file('http://'+url)
+            print('Success: '+'http://'+url)
+    else:
+        status = check_url(url)
+        if status is not None:
+            write_file(url)
+            print('Success: '+url)
