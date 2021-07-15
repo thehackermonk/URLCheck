9d48a6d3f84c299be2d7e67968c9a6a08b6a33fe
Karthik Prakash <karthik.prakash1998@gmail.com>
2021-07-15 14:54:21 +0530

    include url directly from terminal.
    URLCheck 2.0
    
    More options were added like the option to add a protocol, print output, full check, etc.

--- urlcheck.py
+++ urlcheck.py
@@ -1,51 +1,119 @@
+import getopt
+import sys
 import urllib.request
 
-## function open file
+## checks if url is working and returns response status code
+def check_url(url,time):
+   try:
+      if(urllib.request.urlopen(url,timeout=time)):
+         status=urllib.request.urlopen(url).getcode()
+         return status
+   except:
+      print("Error: "+url)
 
+## function to open input url list
 def open_file(file_name):
-    with open(file_name) as f:
-        mylist = list(f)
-        return mylist
+   with open(file_name) as f:
+      return list(f)
 
-## function to append to file
-
+## function to write output to file
 def write_file(text):
-    with open('output.txt', "a") as myfile:
-        myfile.write(text+"\n")
+   with open('output.txt',"a") as myfile:
+      myfile.write(text+"\n")
 
-## function to check url and return status
+## main function
+def main(argv):
 
-def check_url(url):
-    try:
-        if(urllib.request.urlopen(url, timeout=10)):
-            status = urllib.request.urlopen(url).getcode()
-            return status
-    except:
-        # catch the exception if the url returns an error
-        print('Error: '+url)
+   # variable declaration
+   url = None
+   urlList = None
+   output = False
+   protocol = None
+   fullCheck = False
+   time = 10  # set default timeout to 10
+
+   # get variable values from terminal
+   try:
+      opts, args = getopt.getopt(argv,"hu:l:op:f",["url=","urllist=","output","protocol=","fullcheck"])
+   except getopt.GetoptError:
+      print("python urlcheck.py -u <url> -p http -o")
+      sys.exit(2)
+   
+   for opt,arg in opts:
+      if opt in ('-h','--help'):
+         print("python urlcheck.py -u <url> -p http -o")
+         sys.exit()
+      elif opt in ('-u','--url'):
+         url = arg
+      elif opt in ('-l','--urllist'):
+         urlList = arg
+      elif opt in ('-o','--output'):
+         output = True
+      elif opt in ('-p','--protocol'):
+         protocol = arg
+      elif opt in ('-f','--fullcheck'):
+         fullCheck = True
+   
+   # if user has not entered any url prompt error message
+   if(url == None and urlList == None):
+      print("Error: You have to enter either url or a list of urls")
+      print("python urlcheck.py -u <url> -p http -o")
+      sys.exit()
+   
+   if(url != None):
+      # if user opted for a full-check, scan urls with all protocols
+      if(fullCheck):
+         protocols = ['https','http','ftp']
+         
+         for protocol in protocols:
+            status = check_url(str(protocol)+"://"+str(url),time)
+
+            if status is not None:
+               print('Success: ',str(protocol)+"://"+str(url))
+               if output:
+                  write_file(str(protocol)+"://"+str(url))
+
+      else:
+         # if user has not opted for full-check and has not mentioned any protocol, set default protocol to https
+         if(protocol == None):
+            protocol = 'https'
+         url = str(protocol)+"://"+str(url)
+         url = url.strip()
+         status = check_url(url,time)
+
+         if status is not None:
+            print('Success: ',url)
+            if output:
+               write_file(url)
+
+   else:
+      for url in open_file(urlList):
+         # if user opted for a-full check, scan urls with all protocols
+         if(fullCheck):
+            protocols = ['https','http','ftp']
+            
+            for protocol in protocols:
+               status = check_url(str(protocol)+"://"+str(url),time)
+
+               if status is not None:
+                  print('Success: ',str(protocol)+"://"+str(url))
+                  if output:
+                     write_file(str(protocol)+"://"+str(url))
+
+         else:
+            # if user has not opted for full-check and has not mentioned any protocol, set default protocol to https
+            if(protocol == None):
+               protocol = 'https'
+            url = str(protocol)+"://"+str(url)
+            print(url)
+            url = url.strip()
+            status = check_url(url,time)
+
+            if status is not None:
+               print('Success: ',url)
+               if output:
+                  write_file(url)
 
 
-# get path of input file
-input_path = input('Enter path of the input file:\n')
-
-for url in open_file(input_path):
-    # strip the url for unwanted characters
-    url = url.strip()
-
-    # complete the url by adding protocol to it, if needed
-    if not url.startswith('https://') or not url.startswith('http://'):
-
-        status = check_url('https://'+url)
-        if status is not None:
-            write_file('https://'+url)
-            print('Success: '+'https://'+url)
-
-        status = check_url('http://'+url)
-        if status is not None:
-            write_file('http://'+url)
-            print('Success: '+'http://'+url)
-    else:
-        status = check_url(url)
-        if status is not None:
-            write_file(url)
-            print('Success: '+url)
+if __name__ == "__main__":
+   main(sys.argv[1:])
